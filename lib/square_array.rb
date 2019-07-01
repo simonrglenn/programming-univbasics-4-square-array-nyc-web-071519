ebad93a2b4367dd4230ed44eed024cc1287d2481
Simon Glenn <simonrglenn@gmail.com>
2019-07-01 01:15:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   i = 0
+  new_array = []
   while i < array.length do
-    array[i] = array[i] * array [i]
+    array[i] = array[i]**2
     i += 1
   end
+  return new_array
 end
